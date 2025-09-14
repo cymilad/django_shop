aa77fb14e5e7c45a98a36c75e7159e269fad1abc
cymilad <milad1418@yahoo.com>
2025-09-15 01:10:53 +0330

    register complate

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,8 @@ app_name = 'accounts'
 urlpatterns = [
     path('login', login_user, name='login'),
     path('logout', logout_user, name='logout'),
-    # path('register', register_user, name='register'),
-    # path('reset-password', reset_password_user, name='reset_password'),
+    path('signup', signup_user, name='signup'),
+    path('reset-password', reset_password_user, name='reset_password'),
+    path('reset-password/<str:token>', password_reset_confirm, name='password_reset_confirm'),
     path('activate/<str:token>/', activate_account, name='activate-account'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.core.signing import TimestampSigner, BadSignature, SignatureExpired
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.urls import reverse
+from django.conf import settings
 from .models import User
 
 
@@ -14,16 +17,16 @@ def activate_account(request, token):
             user.is_verified = True
             user.is_active = True  # Activate after email confirmation.
             user.save()
-            messages.success(request, 'اکانت شما با موفقیت فعال شد! حالا می‌توانید وارد شوید.')
+            messages.success(request, 'اکانت شما با موفقیت فعال شد. اکنون می‌توانید وارد شوید.')
             print('Your account has been successfully activated. You can now log in.')
         else:
             messages.info(request, 'اکانت شما قبلاً فعال شده بود.')
             print('Your account was already activated.')
-        return redirect('website:index')  # Redirect Home page
+        return redirect('accounts:login')  # Redirect login page
     except (BadSignature, SignatureExpired, User.DoesNotExist):
         messages.error(request, 'لینک فعالسازی نامعتبر یا منقضی شده است')
         print('The activation link is invalid or expired.')
-        return redirect('website:index')
+        return redirect('accounts:login')
 
 
 def login_user(request):
@@ -46,15 +49,80 @@ def login_user(request):
 
         if user is not None:
             login(request, user)
-            messages.success(request, 'با موفقیت وارد شدید.')
+            print('successfully logged in.')
             return redirect('website:index')
         else:
-            messages.error(request, "ایمیل یا رمزعبور اشتباه است.")
+            messages.error(request, "ایمیل یا رمز عبور اشتباه است.")
 
     return render(request, 'accounts/login.html')
 
 
 def logout_user(request):
     logout(request)
-    messages.success(request, "با موفقیت خارج شدید")
+    messages.success(request, "شما با موفقیت خارج شدید.")
     return redirect("accounts:login")
+
+
+def signup_user(request):
+    if request.method == 'POST':
+        email = request.POST.get('email', '').strip()
+        password = request.POST.get('password', '').strip()
+        confirm_password = request.POST.get('confirm_password', '').strip()
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        phonenumber = request.POST.get('phonenumber', '').strip()
+
+        form_data = {
+            'email': email,
+            'firstname': firstname,
+            'lastname': lastname,
+            'phonenumber': phonenumber
+        }
+
+        # validators
+        if not all([email, password, confirm_password, firstname, lastname, phonenumber]):
+            messages.error(request, "لطفاً همه فیلدها را پر کنید.")
+            return render(request, 'accounts/signup.html', {'form_data': form_data})
+
+        if len(password) < 8:
+            messages.error(request, 'رمز عبور باید حداقل ۸ کاراکتر باشد.')
+            return render(request, 'accounts/signup.html', {'form_data': form_data})
+
+        if password != confirm_password:
+            messages.error(request, 'رمز عبور و تکرار با هم مطابقت ندارد')
+            return render(request, 'accounts/signup.html', {'form_data': form_data})
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'این ایمیل قبلاً ثبت شده است')
+            return render(request, 'accounts/signup.html', {'form_data': form_data})
+
+        # make user disable_active
+        user = User.objects.create_user(email=email, password=password)
+
+        # complate Profile user
+        profile = user.user_profile
+        profile.first_name = firstname
+        profile.last_name = lastname
+        profile.phone_number = phonenumber
+        profile.save()
+
+        # send email active user
+        signer = TimestampSigner()
+        token = signer.sign(user.pk)
+        activation_link = f"{settings.SITE_URL}{reverse('accounts:activate-account', kwargs={'token': token})}"
+
+        subject = 'فعالسازی حساب کاربری'
+        message = f"سلام {firstname}\nبرای فعالسازی حساب خود روی لینک زیر کلیک کنید:\n{activation_link}"
+        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [email])
+
+        messages.success(request, 'ثبت ‌نام انجام شد. لطفاً ایمیل خود را برای فعالسازی بررسی کنید.')
+        return redirect('accounts:signup')
+
+    return render(request, 'accounts/signup.html')
+
+
+def reset_password_user(request):
+    return render(request, 'accounts/reset_password.html')
+
+def password_reset_confirm(request, token):
+    return render(request, 'accounts/reset_password_confirm.html')
