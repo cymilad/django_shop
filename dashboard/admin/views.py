a63d01a3ce15bd49cd576d4120e7cdf635205c6e
cymilad <milad1418@yahoo.com>
2025-10-10 00:37:01 +0330

    complate admin dashboard and footer and hader includes

--- dashboard/admin/views.py
+++ dashboard/admin/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
+from functools import wraps
+from accounts.models import UserType
 
 
-@login_required(login_url=reverse_lazy('accounts:login'))
+def admin_required(view_func):
+    @wraps(view_func)
+    @login_required(login_url='accounts:login')
+    def warapped_view(request, *args, **kwargs):
+        if request.user.type != UserType.superuser.value and request.user.type != UserType.admin.value:
+            return HttpResponseForbidden("شما دسترسی ادمین ندارید")
+        return view_func(request, *args, **kwargs)
+
+    return warapped_view
+
+
+@admin_required
 def admin_dashboard_home(request):
     data = {}
     return render(request, 'dashboard/admin/home.html', data)
--- dashboard/customer/views.py
+++ dashboard/customer/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
+from functools import wraps
+from accounts.models import UserType
 
 
-@login_required(login_url=reverse_lazy('accounts:login'))
+def customer_required(view_func):
+    @wraps(view_func)
+    @login_required(login_url='accounts:login')
+    def _wrapped_view(request, *args, **kwargs):
+        if request.user.type != UserType.customer.value:
+            return HttpResponseForbidden("شما دسترسی مشتری ندارید")
+        return view_func(request, *args, **kwargs)
+
+    return _wrapped_view
+
+
+@customer_required
 def customer_dashboard_home(request):
     data = {}
     return render(request, 'dashboard/customer/home.html', data)
